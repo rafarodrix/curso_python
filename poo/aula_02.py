bab388c5b20f156c1ed124a80f3e3e2191dfad88
rafarodrix <rafaelrodrix@gmail.com>
2022-04-28 14:51:52 -0300

    Abstração Instancia

--- poo/aula_02.py
+++ poo/aula_02.py
@@ -1,1 +1,22 @@
-"""teste git"""
+class Fila:
+
+    """
+        Abstração de qualquer tipo de Fila:
+        - Supermercado
+        - Processos
+    """
+
+    c_fila = []
+
+    @classmethod
+    def c_entrar(cls, obj):
+        cls.c_fila.append(obj)
+        print(cls.c_fila)
+
+    def __init__(self):
+        self.s_fila = []
+
+    """Instancia ou exemplo"""
+    def s_entrar(self, obj):
+        self.s_fila.append(obj)
+        print(self.s_fila)
