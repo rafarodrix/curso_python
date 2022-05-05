2f6b2c7ff64b70ec2e0ce66f845dbdc317dae804
rafarodrix <rafaelrodrix@gmail.com>
2022-05-05 19:14:32 -0300

    Ajustes

---
+++ Aulas/introducao_python/Explicacao/print_input.py
@@ -0,0 +1,10 @@
+# print <- Função que escreve na tela, quando chamada
+
+print('Oi meu nome é rafael\n')
+print('Meu nome é Marcos')
+
+# input <- Escreve uma mensagem e espera que você escreva algo
+
+idade = input('Qual sua idade?')
+print(idade)
+
---
+++ Aulas/introducao_python/exercicio/01_print_input.py
@@ -0,0 +1,4 @@
+nome = input('Qual seu nome?\n')
+idade = input('Qual sua idade?\n')
+
+print (f'Bem vindo {nome} fico feliz em saber que tem {idade} anos!')
